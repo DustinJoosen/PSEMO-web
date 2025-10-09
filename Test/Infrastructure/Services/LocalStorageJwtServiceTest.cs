d96084c27ce1c9400f439e2153ea1de398498b00
DustinJoosen <dustinjoosen2003@gmail.com>
2025-10-09 12:48:02 +0200

    Unit tests

---
+++ Test/Infrastructure/Services/LocalStorageJwtServiceTest.cs
@@ -0,0 +1,46 @@
+using Infrastructure.Services;
+using Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Test.Infrastructure.Services;
+
+
+[TestClass]
+public class LocalStorageJwtServiceTests
+{
+    private Mock<ILocalStorageJwtService> _localStorageJwtService = new Mock<ILocalStorageJwtService>();
+
+    //[TestMethod]
+    //public async Task HasReturnsTrue()
+    //{
+    //    // Arrange.
+    //    ILocalStorageJwtService service = this._localStorageJwtService.Object;
+    //    //await service.Remove();
+    //    await service.Set("XXX");
+
+    //    // Act.
+    //    var has = await service.Has();
+
+    //    Console.WriteLine(service);
+    //    Console.WriteLine(has);
+
+    //    // Assert.
+    //    Assert.IsTrue(has);
+    //}
+
+    [TestMethod]
+    public async Task Remove()
+    {
+        // Arrange.
+        ILocalStorageJwtService service = this._localStorageJwtService.Object;
+
+        // Act.
+        await service.Remove();
+
+        // Assert.
+        Assert.AreEqual(await service.Has(), false);
+    }
+
+
+}
