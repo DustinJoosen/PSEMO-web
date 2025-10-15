3a1bdeec5a4914fb39a4f08e424d7ee910c4a5b9
DustinJoosen <dustinjoosen2003@gmail.com>
2025-10-15 10:33:34 +0200

    Rename EditableMap and have it display where you clicked

--- Frontend/wwwroot/js/map.js
+++ Frontend/wwwroot/js/map.js
@@ -1,58 +1,81 @@
 ﻿
-// Global variables holding the viewable and clickable maps.
+// Global variables holding the viewable and clickable maps, and their layer groups.
+// The layer groups contain all the markers.
 window.viewableMap = null;
-window.editableMap = null;
+window.viewableMapLayerGroup = null;
+
+window.coordinateMap = null;
+window.coordinateMapLayerGroup = null;
 
 // This one is here for the javascript->blazor connection.
 window.setDotNetHelper = (value) => {
     window.dotnetHelper = value;
 }
 
-
-// Call this to initialize a viewing map, at a given latitude and longitude.
+// Call this to initialize a view map, at a given latitude and longitude.
 window.initializeViewableMap = (lat, lng) => {
     if (window.viewableMap != null) {
         return;
     }
 
     window.viewableMap = L.map("viewable-map").setView([lat, lng], 12);
+    window.viewableMapLayerGroup = L.layerGroup().addTo(window.viewableMap);
 
     L.tileLayer("https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
         subdomains: ["mt0", "mt1", "mt2", "mt3"],
     }).addTo(window.viewableMap);
 }
 
-// Call this to initialize an editing map, at a given latitude and longitude.
-window.initializeEditableMap = (lat, lng) => {
-    if (window.editableMap != null) {
+// Call this to initialize a coordinate map, at a given latitude and longitude.
+window.initializeCoordinateMap = (lat, lng) => {
+    if (window.coordinateMap != null) {
         return;
     }
 
-    window.editableMap = L.map("editable-map").setView([lat, lng], 12);
+    window.coordinateMap = L.map("coordinate-map").setView([lat, lng], 12);
+    window.coordinateMapLayerGroup = L.layerGroup().addTo(window.coordinateMap);
 
     L.tileLayer("https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}", {
         subdomains: ["mt0", "mt1", "mt2", "mt3"],
-    }).addTo(window.editableMap);
+    }).addTo(window.coordinateMap);
 
-    window.editableMap.addEventListener("click", function (e) {
+    // When you click somewhere, invoke the OnMapSelect in blazor with the coordinates of that click.
+    window.coordinateMap.addEventListener("click", function (e) {
         var coord = e.latlng;
         var lat = coord.lat;
         var lng = coord.lng;
 
-        window.dotnetHelper.invokeMethodAsync("OnMapSelect", lat, lng)
+        // Give the coordinates back to blazor.
+        window.dotnetHelper.invokeMethodAsync("OnMapSelect", lat, lng);
 
+        // Set a marker at where you clicked.
+        window.clearMapMarkers(true);
+        window.addMapMarker(lat, lng, null, true);
     });
 }
 
+// Remove all markers from the global map. True->coordinatemap. False->viewablemap.
+window.clearMapMarkers = (isCoordinateMap) => {
+    var layerGroup = isCoordinateMap
+        ? window.coordinateMapLayerGroup
+        : window.viewableMapLayerGroup;
 
+    layerGroup.clearLayers();
+}
 
-// Add a marker to the global map.
-window.addMapMarker = (lat, lng, title) => {
-    if (!window.editableMap) {
+// Add a marker to the map. True->coordinatemap. False->viewablemap
+window.addMapMarker = (lat, lng, title, isCoordinateMap = false) => {
+    var layerGroup = isCoordinateMap
+        ? window.coordinateMapLayerGroup
+        : window.viewableMapLayerGroup;
+
+    if (!layerGroup) {
         console.error("Marker added before the map was initialized");
         return;
     }
 
-    let marker = L.marker([lat, lng]).addTo(window.editableMap);
-    marker.bindPopup("<b>" + title + "</b>")
+    let marker = L.marker([lat, lng]).addTo(layerGroup);
+    if (title != null) {
+        marker.bindPopup("<b>" + title + "</b>")
+    }
 }
